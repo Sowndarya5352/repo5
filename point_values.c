23a839d0b0d81b68938bb1affb332356c57dad6a
Sowndarya5352 <sowndaryas.ca21@kascsathy.ac.in>
2023-07-31 10:04:13 +0000

    first commit

---
+++ point_values.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+        float a;
+        printf("Enter a floating point value:");
+        scanf("%f",&a);
+        printf("The number entered is %f\n",a);
+        return 0;
+}
